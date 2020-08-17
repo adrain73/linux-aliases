441a0edf35ddf7ef489712351404aa2c6a4eac5c
Adraelsen <adrianrayishmael@gmail.com>
2020-08-16 22:33:35 -0500

    added now to reboot and shutdown command to fix that bug

--- aliases.sh
+++ aliases.sh
@@ -2,8 +2,8 @@
 alias c="clear"
 alias e="exit"
 alias ll="ls -l"
-alias sn="sudo shutdown"
-alias rn="sudo reboot"
+alias sn="sudo shutdown now"
+alias rn="sudo reboot now"
 
 # arch specific aliases
 alias pacR="sudo pacman -Rns"
